0d1d8150c767f735064b183140aa58458cfbe579
Julio Mendoza <juliomendoza.medellin@gmail.com>
2020-05-25 20:31:19 -0500

    Mejoras en getDetalleCuatrieniosComuna, Nueva API

--- src/app.js
+++ src/app.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const EventEmitter = require('events');
+
+
 const cors = require('cors');
 
 const app = express();
+
+//Register a listener
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
 
+const emitter = new EventEmitter();
+emitter.once('log', () => console.log('log once'));
+
+// Returns a new Array with a function `onceWrapper` which has a property
+// `listener` which contains the original listener bound above
+const listeners = emitter.rawListeners('log');
+const logFnWrapper = listeners[0];
+
+// Logs "log once" to the console and does not unbind the `once` event
+logFnWrapper.listener();
+
+// Logs "log once" to the console and removes the listener
+logFnWrapper();
+
+emitter.on('log', () => console.log('log persistently'));
+// Will return a new Array with a single function bound by `.on()` above
+const newListeners = emitter.rawListeners('log');
+
+// Logs "log persistently" twice
+newListeners[0]();
+emitter.setMaxListeners(0)
+emitter.emit('log');
 
 module.exports =app;
 
