9c4faa4012652397b29ad52a0fb81a98571fc274
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 17:14:49 -0500

    nuevo timeput

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.url,
   max: 10000,
-  idleTimeoutMillis: 40000000000,
-  connectionTimeoutMillis: 4000000000,
+  idleTimeoutMillis: 30000000000,
+  connectionTimeoutMillis: 3000000000,
 
   ssl: { rejectUnauthorized: false }
 });
