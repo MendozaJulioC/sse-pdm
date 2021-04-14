01d30f967c481b5c23916a9d7edf33a0b5d0ea85
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 18:40:05 -0500

    mas

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   connectionString: process.env.url,
    max: 1000,
   idleTimeoutMillis: 40000000,
-  connectionTimeoutMillis: 600000,
+  connectionTimeoutMillis: 6000000,
 
   ssl: { rejectUnauthorized: false }
 });
