d76827dc88b22fa26a0c50b5c6b6729ec4e84673
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 18:38:35 -0500

    aotra vez

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -6,8 +6,8 @@ const isProduccion = process.env.NODE_ENV=== "production";
 const pool = new Pool({
   connectionString: process.env.url,
    max: 1000,
-  idleTimeoutMillis: 300000000,
-  connectionTimeoutMillis: 100000000,
+  idleTimeoutMillis: 40000000,
+  connectionTimeoutMillis: 600000,
 
   ssl: { rejectUnauthorized: false }
 });
