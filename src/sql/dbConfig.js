958ebfa0d4efe7aa416fd0a932858666f7cfd1e7
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 18:49:04 -0500

    otra version timeout

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -6,8 +6,9 @@ const isProduccion = process.env.NODE_ENV=== "production";
 const pool = new Pool({
   connectionString: process.env.url,
    max: 1000,
-  idleTimeoutMillis: 40000000,
-  connectionTimeoutMillis: 6000000,
+   idleTimeoutMillis: 10000, // Time to keep a connection idle. Default is 10s
+  waitForAvailableConnectionTimeoutMillis: 90000, // Time to wait to obtain a connection from the pool. Default is 90s
+  connectionTimeoutMillis: 30000, // Max time to connect to postgres. Default is 30s
 
   ssl: { rejectUnauthorized: false }
 });
