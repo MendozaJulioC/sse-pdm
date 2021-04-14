0156c83a526ffdaa7e969266194b14c81a97eac9
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 18:53:03 -0500

    nuevamente

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -6,7 +6,7 @@ const isProduccion = process.env.NODE_ENV=== "production";
 const pool = new Pool({
   connectionString: process.env.url,
    max: 1000,
-   idleTimeoutMillis: 10000, // Time to keep a connection idle. Default is 10s
+   idleTimeoutMillis: 30000, // Time to keep a connection idle. Default is 10s
   waitForAvailableConnectionTimeoutMillis: 90000, // Time to wait to obtain a connection from the pool. Default is 90s
   connectionTimeoutMillis: 30000, // Max time to connect to postgres. Default is 30s
 
