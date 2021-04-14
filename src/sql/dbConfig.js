78a6327503fe353294f5dab2d3c765935dc2d408
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 18:19:14 -0500

    otra

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -1,14 +1,12 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.url,
-  host: dbSocketAddr[0], // e.g. '127.0.0.1'
-  port: dbSocketAddr[1], // e.g. '5432'
-  max: 20,
+   max: 20,
   //acquireTimeoutMillis: 0,
- // createTimeoutMillis: 0,
+  //createTimeoutMillis: 0,
  
-  idleTimeoutMillis: 0,
-  connectionTimeoutMillis: 0,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 
   ssl: { rejectUnauthorized: false }
 });
