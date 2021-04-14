1c528254b2e1bc5706ca528f7e121757fc046e11
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 17:51:40 -0500

    update time out 4

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.url,
-  max: 20,
-  idleTimeoutMillis: 400000000,
-  connectionTimeoutMillis: 100000000,
+  max: 200,
+  acquireTimeoutMillis: 600000,
+  createTimeoutMillis: 30000,
 
+  idleTimeoutMillis: 600000000,
+ // connectionTimeoutMillis: 100000000,
   ssl: { rejectUnauthorized: false }
 });
 pool.connect().then(() => console.log('Conex..DB'))
