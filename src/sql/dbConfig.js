a56a03541cb2cac5e05b286ff22357d301fd552e
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 20:25:42 -0500

    OTRA MAS

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -3,9 +3,9 @@ const pool = new Pool({
   connectionString: process.env.url,
   max: 20,
   idleTimeoutMillis: 400000000000,
-  connectionTimeoutMillis: 6000000,
+  connectionTimeoutMillis: 60000000,
 
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: true }
 });
 pool.connect().then(() => console.log('Conex..DB'))
 
