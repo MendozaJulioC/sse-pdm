47f8db9f05374c8cfdcc44a6f540f833d83c5bf0
Julio Mendoza <juliomendoza.medellin@gmail.com>
2021-04-14 20:18:08 -0500

    hola

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.url,
   max: 20,
-  //idleTimeoutMillis: 400000000000000,
+  idleTimeoutMillis: 400000000000,
   connectionTimeoutMillis: 6000000,
 
   ssl: { rejectUnauthorized: false }
