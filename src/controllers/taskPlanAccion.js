5be6a632a0dc02f7ea92c1880a4e8b9d56c39e99
MendozaJulioC <43477339+MendozaJulioC@users.noreply.github.com>
2021-11-05 08:44:11 -0500

    update query alerta fisica

--- src/controllers/taskPlanAccion.js
+++ src/controllers/taskPlanAccion.js
@@ -347,7 +347,7 @@ const getAlertaFinanciera= async  (req, res)=>{
 const getAlertaFisica = async(req, res)=>{
     try {
         const alerta = req.params.alerta
-        const response = await pool.query(`select * from plan_accion.tbl_exec_fisica where porc_eficacia_proyecto<$1 num_valstat > 0`, [alerta])
+        const response = await pool.query(`select * from plan_accion.tbl_exec_fisica where porc_eficacia_proyecto<$1 and num_valstat > 0`, [alerta])
         res.status(200). json({
             Autor:'Alcaldía de Medellin - Departamento Administrativo de Planeación ',
             Version: '1.0',
