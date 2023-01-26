c3aa41b41348bcc8ebee535862d96e82f1bce43c
MendozaJulioC <juliomendoza.medellin@gmail.com>
2023-01-26 17:22:01 -0500

    cambio conexion db local eliminada

--- src/sql/dbConfig.js
+++ src/sql/dbConfig.js
@@ -22,7 +22,7 @@ const pool2 = new Pool ({
 pool.connect().then(() => console.log('Conex heroku2..DB'))
 */
 
-
+/*
 const local_pool = new Pool ({
   user: 'postgres',
   host: 'localhost',
@@ -35,6 +35,7 @@ const local_pool = new Pool ({
 
 });
 local_pool.connect().then(() => console.log('Conex Local_DB'))
+*/
 
 const aws_pool = new Pool ({
   user: 'subpiee',
@@ -49,4 +50,4 @@ const aws_pool = new Pool ({
 })
 aws_pool.connect().then(() => console.log('Conex ws..DB'))
 
-module.exports = { local_pool, aws_pool };
+module.exports = {  aws_pool };
